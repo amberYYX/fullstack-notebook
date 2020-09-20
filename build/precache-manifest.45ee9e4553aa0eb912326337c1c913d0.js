a6e8e798439bdab3f1bf2c779ae3923c57fdc70b
Yyx <31329477+amberYYX@users.noreply.github.com>
2020-09-20 11:23:51 +0200

    build update

---
+++ build/precache-manifest.45ee9e4553aa0eb912326337c1c913d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119b884fae61147d846e0f84c53ba1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f58c1914a2b1edfd6e36",
+    "url": "/static/css/main.40077fd3.chunk.css"
+  },
+  {
+    "revision": "6ea713f67daeacd02f78",
+    "url": "/static/js/2.ba86cdb7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ba86cdb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58c1914a2b1edfd6e36",
+    "url": "/static/js/main.8525d21f.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
